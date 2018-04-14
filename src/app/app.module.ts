29e3fb2ea716e10531fcc055c67d3e7db5fd6f64
mattdyer <madmatt1220@gmail.com>
2018-04-13 22:15:42 -0600

    added routing and some test pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home.component';
+import { SubpageComponent } from './pages/subpage.component';
+import { NotFoundComponent } from './pages/notfound.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'subpage', component: SubpageComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SubpageComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/notfound.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './notfound.html'
+})
+export class NotFoundComponent {
+
+}
