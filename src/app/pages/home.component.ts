7f50bb9d7cd609cccf7e8bc904b092faeb8d2a69
Matt Dyer <madmatt1220@gmail.com>
2020-07-14 20:08:59 -0600

    removing old files

--- src/app/pages/home.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './home.html'
-})
-export class HomeComponent {
-  
-}
--- src/app/pages/notfound.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './notfound.html'
-})
-export class NotFoundComponent {
-
-}
